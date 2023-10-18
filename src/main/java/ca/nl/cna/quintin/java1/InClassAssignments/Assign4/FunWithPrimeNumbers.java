f951d99d355609b5dd7030547387d3250b7425b3
QAPT12 <tuckquintin@hotmail.com>
2023-10-18 08:03:48 -0230

    further solution

--- src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign4/FunWithPrimeNumbers.java
+++ src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign4/FunWithPrimeNumbers.java
@@ -8,13 +8,53 @@ public class FunWithPrimeNumbers {
 
     public static void main(String[] args) {
 
-        System.out.println("'Fun' with Prime Numbers!");
-        System.out.println("Enter number to check if prime: ");
-
         Scanner input = new Scanner(System.in);
 
-        int userNum = input.nextInt();
+        System.out.println("'Fun' with Prime Numbers!");
+        System.out.println("""
+                1. Print all primes between 1 and a number.
+                2. Print all primes between two numbers.
+                3. Print unique prime factorization of a number.
+                4. Print unique prime factorization of all numbers up to a limit.
+                """);
 
-        System.out.printf("%n is prime: %b", isPrime(userNum));
+        System.out.println("Enter choice: ");
+
+        int userChoice = input.nextInt();
+
+        if(userChoice == 1){
+
+            System.out.print("Enter a number: ");
+
+            int userNum = input.nextInt();
+
+            for (int i = 1; i <= userNum ; i++) {
+                if(isPrime(i)){
+                    System.out.printf("%d ", i);
+                }
+            }
+        } else if (userChoice == 2) {
+
+            System.out.print("Enter first number: ");
+            int startNum = input.nextInt();
+
+            System.out.println("Enter second number: ");
+            int endNum = input.nextInt();
+
+            for (int i = startNum; i <= endNum; i++) {
+                if(isPrime(i)){
+                    System.out.printf("%d ", i);
+                }
+            }
+        } else if (userChoice == 3) {
+
+        }
+//        else if (userChoice == 4) {
+//
+//        } else {
+//
+//        }
+
+
     }
 }
--- src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign4/PrimeNumberCalculator.java
+++ src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign4/PrimeNumberCalculator.java
@@ -43,7 +43,7 @@ public class PrimeNumberCalculator {
         if (n % 2 == 0 || n % 3 == 0)
             return false;
 
-        for (int i = 5; i <= Math.sqrt(n); i = i + 6)
+        for (int i = 5; i <= Math.sqrt(n); i += 6)
             if (n % i == 0 || n % (i + 2) == 0)
                 return false;
 
