6bcaa96b840825c30fbe848cdefb3c1b311196eb
quintin.tuck <tuckquintin@hotmail.com>
2023-11-01 11:32:08 -0230

    Assignment 5 Java docs added

--- src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign5/InvoiceItem.java
+++ src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign5/InvoiceItem.java
@@ -1,33 +1,63 @@
 package ca.nl.cna.quintin.java1.InClassAssignments.Assign5;
 
+/**
+ * Class representing a line item on an invoice for the simple invoice system.
+ *
+ * @author quintin.tuck
+ */
 public class InvoiceItem {
 
     private Product product;
     private int quantity;
 
+    /**
+     * Constructor for the invoice item. Takes a Product object and the quantity.
+     * @param product item of type Product.
+     * @param quantity amount of that product to add to the invoice.
+     */
     public InvoiceItem(Product product, int quantity) {
         this.product = product;
         this.quantity = Math.max(quantity, 0);
     }
 
+    /**
+     * Get product
+     * @return the product object of the invoice line item.
+     */
     public Product getProduct() {
         return product;
     }
 
+    /**
+     * Set product
+     * @param product item of type Product for the invoice line item.
+     */
     public void setProduct(Product product) {
         this.product = product;
     }
 
+    /**
+     * Get quantity
+     * @return quantity of the product being added to the invoice line item.
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Set quantity
+     * @param quantity quantity of the item being added to the invoice line item.
+     */
     public void setQuantity(int quantity) {
         if(quantity >= 0){
             this.quantity = quantity;
         }
     }
 
+    /**
+     * Get item total
+     * @return total price for the invoice line item. Quantity * price of product.
+     */
     public double getItemTotal() {
         double price = this.product.getPrice();
         int quantity = this.quantity;
--- src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign5/Product.java
+++ src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign5/Product.java
@@ -1,39 +1,75 @@
 package ca.nl.cna.quintin.java1.InClassAssignments.Assign5;
 
+/**
+ * Class representing a product for the simple invoice system.
+ *
+ * @author quintin.tuck
+ */
 public class Product {
 
     private int id;
     private String description;
     private double price;
 
+    /**
+     * Constructor for the product. Takes a product id, description of product, and price.
+     * @param id id for product
+     * @param description product description
+     * @param price price of product. Takes the highest value between the given value and 0 to not allow negatives.
+     */
     public Product(int id, String description, double price) {
         this.id = id;
         this.description = description;
         this.price = Math.max(price, 0);
     }
 
+    /**
+     * Get product id.
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Set product id.
+     * @param id product id. Must be greater than zero. if ont id does not change.
+     *
+     */
     public void setId(int id) {
         if(id >= 0) {
             this.id = id;
         }
     }
 
+    /**
+     * Get product description.
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set description.
+     * @param description products description.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Get price.
+     * @return price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Set price.
+     * @param price price of product. Must be greater than 0 or price is not changed.
+     */
     public void setPrice(double price) {
         if(price >= 0) {
             this.price = price;
