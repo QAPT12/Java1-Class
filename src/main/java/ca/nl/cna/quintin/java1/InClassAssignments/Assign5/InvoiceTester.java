288dbf5c00b8a193d27dd48a839098dca5b83986
quintin.tuck <tuckquintin@hotmail.com>
2023-11-01 10:32:32 -0230

    Assignment 5

--- src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign5/InvoiceTester.java
+++ src/main/java/ca/nl/cna/quintin/java1/InClassAssignments/Assign5/InvoiceTester.java
@@ -7,19 +7,22 @@ public class InvoiceTester {
         Product socks = new Product(1, "a pair of socks", 3.99);
         Product mud = new Product(2, "some mud. not worth much", -2.14);
 
-        System.out.printf("\nProduct id: %d. Description: %s. Price: %.2f",
+        System.out.println("creating a product socks, price = 3.99");
+        System.out.printf("Product id: %d. Description: %s. Price: %.2f\n",
                 socks.getId(),
                 socks.getDescription(),
                 socks.getPrice());
 
-        System.out.printf("\nProduct id: %d. Description: %s. Price: %.2f\n",
+        System.out.println("\ncreating a product mud, price = -2.14");
+        System.out.printf("Product id: %d. Description: %s. Price: %.2f\n",
                 mud.getId(),
                 mud.getDescription(),
                 mud.getPrice());
 
+        System.out.println("\nsetting the price of mud to 0.99");
         mud.setPrice(0.99);
 
-        System.out.printf("\nnew price of mud %.2f\n", mud.getPrice());
+        System.out.printf("new price of mud %.2f\n", mud.getPrice());
 
         InvoiceItem socksLineItem = new InvoiceItem(socks, 3);
         InvoiceItem mudLineItem = new InvoiceItem(mud, 2);
@@ -36,10 +39,10 @@ public class InvoiceTester {
 
         Invoice invoice = new Invoice();
 
-        invoice.setInvoiceItem1(socksLineItem);
-        invoice.setInvoiceItem2(mudLineItem);
+        invoice.addInvoiceLineItem(socksLineItem);
+        invoice.addInvoiceLineItem(mudLineItem);
 
-        System.out.printf("\nInvoice Total = %.2f", invoice.getInvoiceTotal());
+        System.out.printf("\nInvoice total: %.2f", invoice.getInvoiceTotal());
 
 
     }
